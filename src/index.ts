8ca69e2f6a3917744a33e053093fde0a2a300ada
HADMARINE <tsx@kakao.com>
2021-06-04 10:02:27 +0900

    feat: custom name function

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,20 @@
 import chalk from "chalk";
 import moment from "moment";
 
-function logger(message: string, isError = false): void {
+function logger(
+  message: string,
+  settings?: { isError?: boolean; appName?: string }
+): void {
   if (process.env.NODE_ENV === "test") {
     return;
   }
-  const out: Function = isError ? console.error : console.log;
+  const out: Function = settings?.isError ? console.error : console.log;
   out(
-    `${chalk.gray(`[${moment().format("YYYY-MM-DD HH:mm:ss")}]: `)}${message}`
+    `${chalk.gray(
+      `[${moment().format("YYYY-MM-DD HH:mm:ss")}]${
+        settings?.appName ? ` [${settings.appName}]` : ``
+      }: `
+    )}${message}`
   );
 }
 
@@ -26,7 +33,9 @@ function warn(...message: string[]): void {
 }
 
 function error(...message: string[]): void {
-  logger(`${chalk.bgRed.black(` ERRR `)} ${chalk.red(...message)}`, true);
+  logger(`${chalk.bgRed.black(` ERRR `)} ${chalk.red(...message)}`, {
+    isError: true,
+  });
 }
 
 function info(...message: string[]): void {
@@ -42,6 +51,44 @@ function clear() {
   console.clear();
 }
 
+function setLoggerName(name: string) {
+  return {
+    debug: (message: any, printStack = true) => {
+      if (process.env.NODE_ENV === "production") return;
+      const stack: string = new Error().stack || "_____UNDEFINED_____";
+      logger(
+        `${chalk}${chalk.bgCyan.black(` DEBG `)}${chalk.cyan(
+          ` ${message}${printStack ? chalk.gray(stack.slice(7)) : ""}`
+        )}`,
+        { appName: name }
+      );
+    },
+
+    warn: (...message: string[]) => {
+      logger(`${chalk.bgYellow.black(` WARN `)} ${chalk.yellow(...message)}`, {
+        appName: name,
+      });
+    },
+
+    error: (...message: string[]) => {
+      logger(`${chalk.bgRed.black(` ERRR `)} ${chalk.red(...message)}`, {
+        isError: true,
+        appName: name,
+      });
+    },
+    info: (...message: string[]) => {
+      logger(`${chalk.bgWhite.black(` INFO `)} ${chalk.white(...message)}`, {
+        appName: name,
+      });
+    },
+    success: (...message: string[]) => {
+      logger(`${chalk.bgGreen.black(` SUCC `)} ${chalk.green(...message)}`);
+    },
+    plain: logger,
+    clear,
+  };
+}
+
 export default {
   error,
   warn,
@@ -50,4 +97,5 @@ export default {
   debug: debugLogger,
   success,
   clear,
+  customName: setLoggerName,
 };
