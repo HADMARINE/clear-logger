21656957a5dddcc684a390eae3acca784be173af
HADMARINE <tsx@kakao.com>
2021-06-26 19:49:53 +0900

    Update: fix printing trace, refactored

--- src/index.ts
+++ src/index.ts
@@ -18,44 +18,18 @@ function logger(
   );
 }
 
-function debugLogger(message: any, printStack = true): void {
-  if (process.env.NODE_ENV === "production") return;
-  const stack: string = new Error().stack || "_____UNDEFINED_____";
-  logger(
-    `${chalk.bgCyan.black(` DEBG `)}${chalk.cyan(
-      ` ${message}${printStack ? chalk.gray(stack.slice(7)) : ""}`
-    )}`
-  );
-}
-
-function warn(...message: string[]): void {
-  logger(`${chalk.bgYellow.black(` WARN `)} ${chalk.yellow(...message)}`);
-}
-
-function error(...message: string[]): void {
-  logger(`${chalk.bgRed.black(` ERRR `)} ${chalk.red(...message)}`, {
-    isError: true,
-  });
-}
-
-function info(...message: string[]): void {
-  logger(`${chalk.bgWhite.black(` INFO `)} ${chalk.white(...message)}`);
-}
-
-function success(...message: string[]): void {
-  logger(`${chalk.bgGreen.black(` SUCC `)} ${chalk.green(...message)}`);
-}
-
 function clear() {
   if (process.env.NODE_ENV === "test") return;
   console.clear();
 }
 
-function setLoggerName(name: string) {
+function initialize(name?: string) {
   return {
     debug: (message: any, printStack = true) => {
       if (process.env.NODE_ENV === "production") return;
-      const stack: string = new Error().stack || "_____UNDEFINED_____";
+      const stack: string =
+        (message instanceof Error ? message.stack : new Error().stack) ||
+        "undfined";
       logger(
         `${chalk.bgCyan.black(` DEBG `)}${chalk.cyan(
           ` ${message}${printStack ? chalk.gray(stack.slice(7)) : ""}`
@@ -89,13 +63,15 @@ function setLoggerName(name: string) {
   };
 }
 
+const d = initialize();
+
 export default {
-  error,
-  warn,
-  info,
+  error: d.error,
+  warn: d.warn,
+  info: d.info,
   plain: logger,
-  debug: debugLogger,
-  success,
-  clear,
-  customName: setLoggerName,
+  debug: d.debug,
+  succes: d.success,
+  clear: d.clear,
+  customName: initialize,
 };
